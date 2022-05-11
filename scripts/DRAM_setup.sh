807cfef30fed5a3343816380f67a0d3a6a509914
Erick Cardenas <erick@microbiomeinsights.com>
2022-05-11 12:54:04 -0400

    Create DRAM_setup.sh

---
+++ scripts/DRAM_setup.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+source ./etc/profile.d/conda.sh
+wget https://raw.githubusercontent.com/shafferm/DRAM/master/environment.yaml
+conda env create -f environment.yaml -n DRAM --yes
+conda activate DRAM
+DRAM-setup.py prepare_databases --output_dir DRAM_data --skip_uniref --verbose --threads 10 &> /logs/download_dram.log
+DRAM-setup.py update_description_db
+DRAM-setup.py export_config --output_file /logs/DRAM_config.log
