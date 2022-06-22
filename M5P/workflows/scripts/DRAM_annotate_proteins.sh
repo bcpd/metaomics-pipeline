1e5ad13a9e09ca2cc5d94c60b591c52ac357cd1a
Erick Cardenas <satanicodr@gmail.com>
2022-06-22 01:25:59 -0400

    New dram script that works with proteins

--- M5P/workflows/scripts/DRAM_annotate_proteins.sh
+++ M5P/workflows/scripts/DRAM_annotate_proteins.sh
@@ -6,12 +6,4 @@ conda activate DRAM
 DRAM.py annotate_genes -i '/genomes/*faa'  -o /out/annotations/ --threads 10 --verbose &> /logs/dram_protein_annotation.log
 
 # Run dram distill
-if [ -f /out/annotations/rrnas.tsv ] && [ -f /out/annotations/trnas.tsv ]
-then
-   DRAM.py distill --input_file /out/annotations/annotations.tsv --rrna_path /out/annotations/rrnas.tsv --trna_path /out/annotations/trnas.tsv --output_dir /out/annotations/distill  &> /logs/dram_distil.log
-elif [ -f /out/annotations/rrnas.tsv ]
-then
-   DRAM.py distill --input_file /out/annotations/annotations.tsv --rrna_path /out/annotations/rrnas.tsv --output_dir /out/annotations/distill  &> /logs/dram_distil.log
-else
-   DRAM.py distill --input_file /out/annotations/annotations.tsv --trna_path /out/annotations/trnas.tsv --output_dir /out/annotations/distill  &> /logs/dram_distil.log
-fi
+DRAM.py distill --input_file /out/annotations/annotations.tsv --output_dir /out/annotations/distill  &> /logs/dram_protein_distil.log
