91d10c9f92b235eaffa9b37a0c076f05482add45
Erick Cardenas <satanicodr@gmail.com>
2022-10-28 13:48:55 -0400

    Corrected script so it parses proteins correctly

--- M5P/workflows/scripts/DRAM_annotate_proteins.sh
+++ M5P/workflows/scripts/DRAM_annotate_proteins.sh
@@ -3,7 +3,7 @@ source ./etc/profile.d/conda.sh
 conda activate DRAM
 
 # Run dram annotate
-DRAM.py annotate_genes -i '/genomes/*faa'  -o /out/annotations/ --threads 10 --verbose &> /logs/dram_protein_annotation.log
+DRAM.py annotate_genes -i '/genomes/*.faa'  -o /out/annotations/ --threads 10 --verbose &> /logs/dram_protein_annotation.log
 
 # Run dram distill
 DRAM.py distill --input_file /out/annotations/annotations.tsv --output_dir /out/annotations/distill  &> /logs/dram_protein_distil.log
