63f4bef4246c5a6c597387371397de1980625679
Erick Cardenas <satanicodr@gmail.com>
2023-05-04 11:11:52 -0400

    Updated rules to add genomes
    Updated rules to add genomes from user for metatranscriptomics mapping

--- M5P/m5p.py
+++ M5P/m5p.py
@@ -43,11 +43,7 @@ def get_template():
     'experimental_design': '~treatment',
     'fastq_metagenomics': 'input_mg',
     'fastq_metatranscriptomics':' input_mt/',
-    'genome': {
-        'find_genome_relative': 'false',
-        'genome_file': 'reference_file',
-        'genome_url': 'reference_url',
-        },
+    'genome': 'genome_folder',
     'jobs': '4',
     'mag_annotation': 'dram',
     'max_memory': '128000000000',
@@ -92,6 +88,8 @@ def validate_arguments(config_data):
                 valid_statements = 0
                 print("working directory does not exist")
         elif arg == "experimental_contrast":
+            val = config_data[arg]
+        elif arg == "genome":
             val = config_data[arg]
         elif arg == "experimental_design":
             val = config_data[arg]
@@ -152,6 +150,9 @@ def update_config(args, config_data):
             elif arg == "merged_prefix":
                 val = getattr(args, arg)
                 config_data["merged_prefix"] = val
+            elif arg == "genome":
+                val = getattr(args, arg)
+                config_data["genome"] = val
             elif arg == "metadata_path":
                 val = getattr(args, arg)
                 config_data["metadata_path"] = val
@@ -203,6 +204,7 @@ def main():
     parser.add_argument("-p", "--merged_prefix", default="MergedReads-001", type=str, help="Prefix for merged reads files (string)")
     parser.add_argument("-b", "--bin_all", type=bool, default = True, help="Put all reads files in same bin group (bool)")
     parser.add_argument("-j", "--jobs", default = 4, type = int, help = "number of jobs")
+    parser.add_argument("-f", "--genome", type=str, help = "optional folder with genomes used for metatranscriptomics mapping.")
     parser.add_argument("-c", "--configfile", action='store', type=str, help = "optional yaml file containing all of the above configuration details.")
     parser.add_argument("-a", "--getconfig", action='store_true', help = "Prints a yaml template with the default configuration. Then exits")
     args = parser.parse_args()
