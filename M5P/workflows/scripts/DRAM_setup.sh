7542433f0959e7346bd4a46ab744ea427dbf476b
Erick Cardenas <satanicodr@gmail.com>
2024-03-02 01:31:28 -0500

    Update DRAM_setup.sh
    - Changed yaml file to point to a fixed environment file for DRAM v1.5.0
    - Added line to copy the dram environment to the container
    -Added line to copy the corrected dram setup script to the container to replace a faulty one.

--- M5P/workflows/scripts/DRAM_setup.sh
+++ M5P/workflows/scripts/DRAM_setup.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 source ./etc/profile.d/conda.sh
-wget https://raw.githubusercontent.com/shafferm/DRAM/master/environment.yaml
-conda env  create --name DRAM -f environment.yaml
+conda env  create --name DRAM -f /scripts/dram.yaml
 conda activate DRAM
+cp /scripts/DRAM-setup.py /opt/conda/envs/DRAM/bin/DRAM-setup.py  # Copy corrected DRAM setup file
 DRAM-setup.py prepare_databases --output_dir DRAM_data --skip_uniref --verbose --threads 10 &> /logs/download_dram.log
 DRAM-setup.py update_description_db
 DRAM-setup.py export_config --output_file /logs/DRAM_config.log
